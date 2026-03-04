ee4c8d1749c3a09c8a789f57c4d596f95821f623
José Daniel Solano Gracia <daniel120802@gmail.com>
2026-03-03 23:20:49 -0500

    fix

--- index.js
+++ index.js
@@ -17,18 +17,19 @@ function incio(){
 
 async function main(){
     audio.play();
-    incio();
     corazones();
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    for (let i = 0; i < imagenes.length; i++) {
-        const element = imagenes[ imagenes.length - 1 - i];
-        // cambiar la posición y rotación de cada imagen
-        element.style.transform = `translateX(0px) rotate(0deg) translateY(100px) scale(1.2)`;
+    while (true) {
+        incio();
         await new Promise(resolve => setTimeout(resolve, 3000));
-        element.style.transform = `translateX(600px) rotate(10deg) translateY(100px) scale(1.2)`;
-        await new Promise(resolve => setTimeout(resolve, 2000));
+        for (let i = 0; i < imagenes.length; i++) {
+            const element = imagenes[ imagenes.length - 1 - i];
+            // cambiar la posición y rotación de cada imagen
+            element.style.transform = `translateX(0px) rotate(0deg) translateY(100px) scale(1.2)`;
+            await new Promise(resolve => setTimeout(resolve, 3000));
+            element.style.transform = `translateX(600px) rotate(10deg) translateY(100px) scale(1.2)`;
+            await new Promise(resolve => setTimeout(resolve, 2000));
+        }
     }
-    main();
 }
 
 document.querySelector('div.panelArriba').addEventListener('click', async () => {
